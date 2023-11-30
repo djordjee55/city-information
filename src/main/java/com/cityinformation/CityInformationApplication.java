218531637fbca4d9d0ec9bd74951b530c3ddc8be
djordjee55 <djordjenjegic1999@gmail.com>
2023-11-30 22:15:17 +0100

    add initial project structure

---
+++ src/main/java/com/cityinformation/CityInformationApplication.java
@@ -0,0 +1,13 @@
+package com.cityinformation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CityInformationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CityInformationApplication.class, args);
+	}
+
+}
